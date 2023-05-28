bd3e0a7432b1ffb6dc96100580055dd6283e6c52
Sodi7DofS <133899536+Sodi7DofS@users.noreply.github.com>
2023-05-28 17:45:26 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Interfaces;
+
+namespace fight
+{
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public short Damage { get; set; } = 15;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.SendingConsoleCommand += OnSendConsoleCommand;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.SendingConsoleCommand -= OnSendConsoleCommand;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using Exiled.API.Features;
+using Exiled.Events;
+
+namespace fight
+{
+    public class Test : Plugin<Config>
+    {
+        public override string Author => "Sodi7ДЛ";
+        public override string Name => "Fight";
+        public override Version RequiredExiledVersion { get; } = new("6.0.0");
+    }
+
+    private void OnSendConsoleCommand(SendingConsoleCommandEventArgs ev)
+    {
+        if (ev.Name.ToLower() != "fight") return;
+        var player = Player.Get.OrderBy(p => p.DistanceSqr(ev.Player)).FirstOrDefault();
+        if (player == null) return;
+        player.Hurt(15, ev.Player);
+        ev.ReturnMessage = $"You have hitted {player.Nickname}.";
+        return;
+    }
+}
