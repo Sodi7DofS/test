ab810559c1a59b4f1015ce7170fd71ce58c4b1a8
Sodi7DofS <133899536+Sodi7DofS@users.noreply.github.com>
2023-06-03 13:25:24 +0300

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,22 +1,47 @@
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.Events;
+using static fight.command.Command;
 
 namespace fight
 {
-    public class Test : Plugin<Config>
+    public class Fight : Plugin<Config>
     {
         public override string Author => "Sodi7ДЛ";
         public override string Name => "Fight";
         public override Version RequiredExiledVersion { get; } = new("6.0.0");
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.SendingConsoleCommand += OnSendConsoleCommand;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.SendingConsoleCommand -= OnSendConsoleCommand;
+        }
     }
+
+
 
     private void OnSendConsoleCommand(SendingConsoleCommandEventArgs ev)
     {
-        if (ev.Name.ToLower() != "fight") return;
-        var player = Player.Get.OrderBy(p => p.DistanceSqr(ev.Player)).FirstOrDefault();
-        if (player == null) return;
-        player.Hurt(Config.Damage, ev.Player);
-        ev.ReturnMessage = $"You have hitted {player.Nickname}.";
-        return;
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!(sender is PlayerCommandSender))
+            {
+                response = "Only players can use this command";
+                return false;
+            }
+            if (Watcher.hitPlayer == null)
+            {
+                response = "You don`t watch on player";
+                return false;
+            }
+            int dmg = Config.Damage;
+            Watcher.hitPlayer.Hurt(dmg, DamageTypes.Physics);
+            response = "You have hitted somebody";
+            return true;
+        }
     }
 }
